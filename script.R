63a70a5e88d78277c45e00f4e3635fbf0c7977a1
Alyssa Holzer <holzealy@uw.edu>
2017-11-20 15:05:31 -0800

    aggregate landfill data by state

--- script.R
+++ script.R
@@ -11,3 +11,11 @@ power <- read.csv("power.csv")
 water <- read.csv("water.csv")
 
 print(water$State)
+
+landfill_by_state <- landfill %>% select("State", "Waste.in.Place..tons.", "LFG.Collection.System.In.Place.", 
+                                          "LFG.Collected..mmscfd.", "MW.Capacity", "Current.Year.Emission.Reductions..MMTCO2e.yr....Direct") %>% 
+  group_by(State) %>% summarise(total.waste = sum(as.numeric(Waste.in.Place..tons., na.rm = TRUE)), 
+                                total.with.lfg = sum(LFG.Collection.System.In.Place. == "Yes", na.rm = TRUE), 
+                                total.lfg.collected = sum(LFG.Collected..mmscfd., na.rm = TRUE),
+                                total.capacity = sum(MW.Capacity, na.rm = TRUE),
+                                total.reduction = sum(Current.Year.Emission.Reductions..MMTCO2e.yr....Direct, na.rm = TRUE)) 
