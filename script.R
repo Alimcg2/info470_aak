5c112732f7284d9120a9999718a07752eea21dc4
Alison McGuire <=>
2017-11-20 14:43:36 -0800

    selected landfill

--- script.R
+++ script.R
@@ -2,6 +2,11 @@
 library(dplyr)
 
 landfill <- read.csv("landfill.csv")
+landfill <- landfill %>% 
+  select("State", "Longitude", "Latitude", "Ownership.Type", "Year.Landfill.Opened", 
+         "Landfill.Closure.Year", "Waste.in.Place..tons.", "LFG.Collection.System.In.Place?", 
+         "LFG.Collected.(mmscfd)", "LFG.Energy.Project.Type", "Project.Type.Category", "MW.Capacity", 
+         "Current.Year.Emission.Reductions.(MMTCO2e/yr).-.Direct")
 power <- read.csv("power.csv")
 water <- read.csv("water.csv")
 
