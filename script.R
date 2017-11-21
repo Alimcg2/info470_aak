f74cd18624a3b1658a56b44c83068f18cd895aca
Alison McGuire <=>
2017-11-20 18:03:54 -0800

    added folder

--- script.R
+++ script.R
@@ -4,9 +4,12 @@
 # Pulling in data and setup
 
 library(dplyr)
+library(ggplot2)
+library(plotly)
 landfill <- read.csv("landfill.csv")
 power <- read.csv("power.csv")
 water <- read.csv("water.csv")
+pop2017 <- read.csv("pop2017.csv")
 
 
 # ----------------------------------------------------------------------------------------------#
@@ -18,17 +21,36 @@ landfill <- landfill %>%
          LFG.Collected..mmscfd., LFG.Energy.Project.Type, Project.Type.Category, MW.Capacity, 
          Current.Year.Emission.Reductions..MMTCO2e.yr....Direct)
 
-landfill_by_state <- landfill %>% select("State", "Waste.in.Place..tons.", "LFG.Collection.System.In.Place.", 
-                                          "LFG.Collected..mmscfd.", "MW.Capacity", "Current.Year.Emission.Reductions..MMTCO2e.yr....Direct") %>% 
-  group_by(State) %>% summarise(total.waste = sum(as.numeric(Waste.in.Place..tons., na.rm = TRUE)), 
-                                total.with.lfg = sum(LFG.Collection.System.In.Place. == "Yes", na.rm = TRUE), 
-                                total.lfg.collected = sum(LFG.Collected..mmscfd., na.rm = TRUE),
-                                total.capacity = sum(MW.Capacity, na.rm = TRUE),
-                                total.reduction = sum(Current.Year.Emission.Reductions..MMTCO2e.yr....Direct, na.rm = TRUE)) 
-
-num.landfills.per.state <- landfill %>% 
+landfill_by_state <- landfill %>% 
+  select(State, Landfill.Name, Waste.in.Place..tons., LFG.Collection.System.In.Place., 
+         LFG.Collected..mmscfd., MW.Capacity, Current.Year.Emission.Reductions..MMTCO2e.yr....Direct) %>% 
   group_by(State) %>% 
-  summarize(count(Landfill.Name))
+  summarise(num.landfills = n(),
+            total.waste = sum(as.numeric(Waste.in.Place..tons., na.rm = TRUE)), 
+            total.with.lfg = sum(LFG.Collection.System.In.Place. == "Yes", na.rm = TRUE), 
+            total.lfg.collected = sum(LFG.Collected..mmscfd., na.rm = TRUE),
+            total.capacity = sum(MW.Capacity, na.rm = TRUE),
+            total.reduction = sum(Current.Year.Emission.Reductions..MMTCO2e.yr....Direct, na.rm = TRUE)) 
+
+
+# Summary Stats
+
+# Number of Landfills Per State
+num.landfills <- landfill_by_state %>% 
+  select(State, num.landfills)
+# Average waste in landfills per state, total waste in landfills per state
+waste.landfills <- landfill %>% 
+  select(State, Waste.in.Place..tons.) %>% 
+  group_by(State) %>% 
+  summarise(average.waste = mean(as.numeric(Waste.in.Place..tons.), na.rm = TRUE), total.waste = sum(as.numeric(Waste.in.Place..tons., na.rm = TRUE)))
+# Number of Landfills and number of LFG programs, percent of landfills with LFG programs
+num.LFG <- landfill_by_state %>% 
+  select(State, num.landfills, total.with.lfg) %>% 
+  mutate(percent.lfg = (total.with.lfg / num.landfills) * 100)
+# State's percentage of waste over total waste
+percent.waste <- landfill_by_state %>% 
+  select(State, total.waste) %>% 
+  mutate(percent.waste.of.all = (total.waste / sum(total.waste)) * 100)
 
 # ----------------------------------------------------------------------------------------------#
 # Power Data
